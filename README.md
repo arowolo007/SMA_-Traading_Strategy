3e76f194998f1798a1f6bff9a0c8c91e7dcde8f5
dhimar058 <arowolodamilola5@gmail.com>
2021-03-01 13:02:55 +0100

    added rhe first part of the SMA code

---
+++ README.md
@@ -0,0 +1,2 @@
+# trader
+Trading Robot
--- binance_algo.py
+++ binance_algo.py
@@ -8,9 +8,16 @@ client = Client(api_key, api_secret)
 # get market depth
 depth = client.get_order_book(symbol='BNBBTC')
 
+
+# print(depth)
+
 # # place a test market buy order, to place an actual order use the create_order function
 # order = client.create_test_order(
 #     symbol='BNBBTC',
 #     side=Client.SIDE_BUY,
 #     type=Client.ORDER_TYPE_MARKET,
 #     quantity=100)
+
+info = client.get_symbol_info('BNBBTC')
+
+print(info)
